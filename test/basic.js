99ba6355f86467d17b9989c8b12f30ffc1a404b8
C. Scott Ananian <cscott@cscott.net>
2013-02-02 16:59:23 -0500

    Add test cases from the bzip2-1.0.6 source code.

--- test/basic.js
+++ test/basic.js
@@ -2,10 +2,18 @@ var assert = require("assert")
 var decodeBzip = require('../');
 var fs = require('fs');
 
-describe('bzip decode', function(){
+describe('bzip2 basic decode', function(){
   it('should correctly decode our example file', function(){
-      var compressedData = fs.readFileSync('test/basic.txt.bz2');
+      var compressedData = fs.readFileSync('test/sample0.bz2');
       var data = decodeBzip(compressedData);
       assert.equal(data, "This is a test\n");
+  });
+  ['sample0', 'sample1', 'sample2', 'sample3'].forEach(function(f) {
+      it('should correctly decode '+f, function() {
+          var compressedData = fs.readFileSync('test/'+f+'.bz2');
+          var referenceData = fs.readFileSync('test/'+f+'.ref');
+          var data = decodeBzip(compressedData, referenceData.length);
+          assert.equal(data.toString('hex'), referenceData.toString('hex'));
+      });
   });
 });
